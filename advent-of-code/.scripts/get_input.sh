cf171b61a68c2e215e55e08756bb6f4383bcaff2
alexandru-dinu <alex.dinu07@gmail.com>
2025-12-04 11:30:09 +0200

    [aoc] better templating

--- advent-of-code/.scripts/get_input.sh
+++ advent-of-code/.scripts/get_input.sh
@@ -6,6 +6,7 @@ YEAR="$1"
 DAY="$2"
 URL="https://adventofcode.com/$YEAR/day/${DAY##0}/input"
 
-curl -X GET $URL \
-    -H "User-Agent: https://github.com/alexandru-dinu/advent-of-code/blob/main/.scripts/get_input.sh" \
+curl -s \
+    -X GET $URL \
+    -H "User-Agent: https://github.com/alexandru-dinu/programming-challenges/blob/main/advent-of-code/.scripts/get_input.sh" \
     -H "Cookie: session=$AOC_SESSION" \
